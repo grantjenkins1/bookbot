b1fa287b9707533c0e4cdb77b22b37e4a6a898e5
grantjenkins1 <jenkinsgrant2074@gmail.com>
2025-05-07 15:44:08 +0000

    updating main.py

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from stats import get_num_words
 from stats import get_letters
+from stats import sort_dict
+from stats import get_letters_list
+import sys
+
+args = sys.argv
+if len(args) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = args[1]
 
 def get_content(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    content = get_content('books/frankenstein.txt')
+    content = get_content(book_path)
+    #number of words
     words = content.split()
-    print(f"{len(words)} words found in the document")
-    letters = get_letters()
-    print(letters["e"])
+    #dict of letters
+    letters = get_letters_list(content)
+    new_letters = sort_dict(letters)
+
+    print(
+       "============ BOOKBOT ============" + "\n" + 
+       "Analyzing book found at " + book_path + "\n" + 
+       "----------- Word Count ----------" + "\n" + 
+       f"Found {len(words)} total words \n" +
+       "--------- Character Count -------"
+            )
+    for letter in letters:
+        if letter['char'].isalpha():
+            print(letter['char'] + ': ' + str(letter['num']))
+    print('============= END ===============')
 
 main()
