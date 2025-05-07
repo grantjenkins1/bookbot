625d5d52a4ad3b6ce6e95850bc57f59f947eaf53
grantjenkins1 <jenkinsgrant2074@gmail.com>
2025-05-07 15:45:22 +0000

    updating stats.py

--- stats.py
+++ stats.py
@@ -12,14 +12,29 @@ def get_num_words():
     words = get_words() 
     print(f"{len(words)} words found in the document")
 
-def get_letters():
+def get_letters(str_content):
     letters = {}
-    words = get_words()
+    words = str_content.lower()
     for word in words:
         for letter in word:
-            if letters[letter]:
+            if letter in letters:
                 letters[letter] += 1
             else:
                 letters[letter] = 1
     return letters
 
+def get_letters_list(content):
+    letters_list = []
+    letters = get_letters(content)
+    for letter in letters:
+        new_letter = {}
+        new_letter['char'] = letter
+        new_letter['num'] = letters[letter]
+        letters_list.append(new_letter)
+    return letters_list
+
+def sort_on(dict):
+    return dict['num']
+
+def sort_dict(dict):
+    return dict.sort(reverse=True, key=sort_on)
